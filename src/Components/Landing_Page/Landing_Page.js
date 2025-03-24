7ef0035820d9a5c28f8c6fef941de95090ed0172
Davis611 <logdavis18@gmail.com>
2025-03-24 14:09:58 -0400

    IC

--- src/Components/Landing_Page/Landing_Page.js
+++ src/Components/Landing_Page/Landing_Page.js
@@ -1,38 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Landing_Page.css';
-import Landing_page from './components/Landing_Page/Landing_page.html';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 const Landing_Page = () => {
-    return (
-      <main className="hero-section">
-        <section>
-          <div>
-            {/* AOS animation attribute applied here */}
-            <div data-aos="fade-up" className="flex-hero">
-              <h1>
-                Your Health
-                <br />
-                <span className="text-gradient">
-                  Our Responsibility
-                </span>
-              </h1>
-              <div className="blob-cont">
-                <div className="blue blob"></div>
-              </div>
-              <div className="blob-cont">
-                <div className="blue1 blob"></div>
-              </div>
-              <h4>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque at
-                quae ducimus. Suscipit omnis quibusdam non cum rem voluptatem!
-              </h4>
-              <a href="#services" aria-label="Explore services">
-                <button className="button">Get Started</button>
-              </a>
-            </div>
+  useEffect(() => {
+    // Initialize AOS animation when the component mounts
+    const AOS = require('aos');
+    AOS.init();
+  }, []);
+
+  return (
+    <main className="hero-section">
+      <div className="container">
+        {/* Hero Section Content */}
+        <div data-aos="fade-up" className="flex-hero">
+          {/* Main Heading */}
+          <h1>
+            Your Health
+            <br />
+            <span className="text-gradient">Our Responsibility</span>
+          </h1>
+
+          {/* Animated Blob Containers */}
+          <div className="blob-cont">
+            <div className="blue blob"></div>
           </div>
-        </section>
-      </main>
-    );
-  };
+          <div className="blob-cont">
+            <div className="blue1 blob"></div>
+          </div>
+
+          {/* Subheading Text */}
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque at
+            quae ducimus. Suscipit omnis quibusdam non cum rem voluptatem!
+          </h4>
+
+          {/* Call to Action Button */}
+          <a href="#services" aria-label="Explore our services">
+            <button className="button">Get Started</button>
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default Landing_Page;
