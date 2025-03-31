1d3d46a4135c32db3928899427ae255359d19b75
Davis611 <logdavis18@gmail.com>
2025-03-31 11:34:34 -0400

    css nav IC

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 
 import React from 'react';
-import AppointmentFormIC from './Components/AppointmentFormIC/AppointmentFormIC';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from './Components/Navbar/Navbar';
